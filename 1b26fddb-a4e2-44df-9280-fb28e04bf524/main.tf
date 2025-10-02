e0c61dc012add89bdbf7f88246a67433496e9348
Cherukuri Gaurav Sushant <89406732+CGS26@users.noreply.github.com>
2025-10-02 13:51:24 +0530

    Add generated infrastructure code

---
+++ 1b26fddb-a4e2-44df-9280-fb28e04bf524/main.tf
@@ -0,0 +1,91 @@
+resource "aws_vpc" "main" {
+cidr_block           = var.vpc_cidr
+enable_dns_support   = true
+enable_dns_hostnames = true
+
+tags = {
+Name = "MainVPC"
+}
+}
+
+resource "aws_subnet" "public" {
+vpc_id                  = aws_vpc.main.id
+cidr_block              = var.public_subnet_cidr
+availability_zone       = var.availability_zone
+map_public_ip_on_launch = true
+
+tags = {
+Name = "PublicSubnet"
+}
+}
+
+resource "aws_subnet" "private" {
+vpc_id            = aws_vpc.main.id
+cidr_block        = var.private_subnet_cidr
+availability_zone = var.availability_zone
+
+tags = {
+Name = "PrivateSubnet"
+}
+}
+
+resource "aws_internet_gateway" "gw" {
+vpc_id = aws_vpc.main.id
+
+tags = {
+Name = "MainIGW"
+}
+}
+
+resource "aws_nat_gateway" "nat" {
+allocation_id = aws_eip.nat.id
+subnet_id     = aws_subnet.public.id
+
+tags = {
+Name = "NATGateway"
+}
+}
+
+resource "aws_eip" "nat" {
+vpc   = true
+count = 1
+tags = {
+Name = "NATEip"
+}
+}
+
+resource "aws_route_table" "public" {
+vpc_id = aws_vpc.main.id
+
+route {
+cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.gw.id
+}
+
+tags = {
+Name = "PublicRouteTable"
+}
+}
+
+resource "aws_route_table" "private" {
+vpc_id = aws_vpc.main.id
+
+route {
+cidr_block     = "0.0.0.0/0"
+nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+tags = {
+Name = "PrivateRouteTable"
+}
+}
+
+resource "aws_route_table_association" "public" {
+subnet_id      = aws_subnet.public.id
+route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+subnet_id      = aws_subnet.private.id
+route_table_id = aws_route_table.private.id
+}
